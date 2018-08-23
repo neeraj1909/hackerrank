7bb03bb7e8d0dc8e8d82873e12788511eb089dd0
neeraj1909 <neeraj1909@gmail.com>
2018-08-23 13:41:17 +0530

    corrected typo

--- Linked-Lists/merge_two_sorted_linked_list.c
+++ Linked-Lists/merge_two_sorted_linked_list.c
@@ -1,22 +1,26 @@
-SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {    
-    if((head1==NULL)&&(head2==NULL))
+SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) 
+{  
+    struct SinglyLinkedListNode* result = NULL;
+
+    if((head1 == NULL)&&(head2 == NULL))
         return NULL;
        
-    if((head1!=NULL)&&(head2==NULL))
+    if((head1 != NULL)&&(head2 == NULL))
         return head1;
        
-    if((head1 == NULL)&&(head2!=NULL))
+    if((head1 == NULL)&&(head2 != NULL))
         return head2;
     
     
     if(head1 -> data <= head2 -> data)
     {
-        head1 -> next = mergeLists(head1 -> next, head2);
-        return head1;
+        result = head1;
+        result -> next = mergeLists(head1 -> next, head2);
     }
     else
     {
-        head2 -> next = mergeLists(head1, head2 -> next);
-        return head2;
+        result = head2;
+        result -> next = mergeLists(head1, head2 -> next);
     }
+    return result;
 }
